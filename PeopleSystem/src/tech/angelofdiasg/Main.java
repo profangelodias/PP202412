64a6fb74a0ee0049c4fa00909e351c2cafa7f3ab
Prof Angelo F Dias <prof2132@iesp.edu.br>
2024-04-22 21:42:03 -0300

    PeopleSystem GRASP 1

--- PeopleSystem/src/tech/angelofdiasg/Main.java
+++ PeopleSystem/src/tech/angelofdiasg/Main.java
@@ -11,41 +11,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        Cliente cli1 = new Cliente();
+//        Endereco end1 = new Endereco();
+        Dependente dep1 = new Dependente();
+        dep1.setClienteParente(cli1);
+//        cli1.setEndereco(end1);
         Scanner input = new Scanner(System.in);
 
-        int opcaoDeEscolha;
-        do{
-            System.out.println("Escolha a opção de cadastramento\n");
-            System.out.print("1.) Cliente \n");
-            System.out.print("2.) Funcionario\n");
-            System.out.print("3.) Exit\n");
-            System.out.print("\nEscolha a opção digitando o número: ");
-
-            opcaoDeEscolha(input.nextInt());
-
-        } while(true);
-
-    }
-
-    private static void opcaoDeEscolha(int opcaoEscolhida) {
-        Cliente c1 = new Cliente();
-        Funcionario func1 = new Funcionario();
-        switch(opcaoEscolhida){
-
-            case 1:
-                //ações
-                c1.cadastrar();
-                break;
-            case 2:
-                //ações;
-                func1.cadastrar();
-                break;
-            case 3:
-                //ação de sair;
-                break;
-            default:
-                System.out.println("Opçaõ inválida");
-                break;
 
     }
 }
---
+++ PeopleSystem/src/tech/angelofdiasg/pessoas/Dependente.java
@@ -0,0 +1,16 @@
+package tech.angelofdiasg.pessoas;
+
+import java.util.UUID;
+
+public class Dependente {
+    private UUID id;
+    private Cliente ClienteParente;
+
+    public Cliente getClienteParente() {
+        return ClienteParente;
+    }
+
+    public void setClienteParente(Cliente clienteParente) {
+        ClienteParente = clienteParente;
+    }
+}
